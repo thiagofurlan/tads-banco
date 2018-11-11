0cc2462566068563b6e52312a31546d8bbd9e9c7
Thiago Furlan <tsfurlan@gmail.com>
2018-11-11 21:28:41 -0200

    upload

--- folha1.sql
+++ folha1.sql
@@ -29,3 +29,10 @@ FROM
 	JOIN item ON item.venda = tmp0.codigo
 	JOIN produto ON produto.codigo = item.produto
 	GROUP BY vendedor.cpf) as tmp1) as tmp2) as tmp3) as tmp4 ORDER BY 1;
+
+
+-- Começando pela query mais interna, obtenho todas as vendas de Fevereiro/2000 e salvo na tabela temporária tmp0, uso o resultado desta query para fazer um JOIN com a consulta que irá trazer os dados do vendedor, junto com a comissão e o salário bruto através da função de agregação SUM() agrupados pelo CPF do vendedor e salvo o resultado na tabela temporária tmp1.
+
+-- Agora que já temos as informações do vendedor, o valor das vendas de cada vendedor, podemos prosseguir com os cálculos. Através da expressão CASE é calculado o valor de contribuição para o INSS com base nas faixas de salário bruto, o resultado é salvo na tabela temporária tmp2. Com os resultados que estão em tmp2, agora é possível calcular o salário base que será salvo em tmp3.
+
+-- O penúltimo passo é calcular o valor do IRRF que é em cima do salário base, q
